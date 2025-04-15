ecb4da6ed6d9c734e8e52f36787b857c0a9fc30f
Alejandro <alejandro.mata@edu.uah.es>
2025-04-15 20:41:23 +0200

    Repositorio creado.

---
+++ PL2/PL2_JAVA/src/main/java/pl2_java/PL2_JAVA.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package pl2_java;
+
+/**
+ *
+ * @author Alejandro
+ */
+public class PL2_JAVA {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
