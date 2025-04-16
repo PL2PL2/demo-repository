65270bbf5140124608ee58500ac28ed645112e13
Daniel <daniel.guillenr@edu.uah.es>
2025-04-16 12:48:45 +0200

    Cambios de nombre de registros

--- PL2/PL2_JAVA/src/main/java/Pl2_interfaz/Login_Administrador.java
+++ PL2/PL2_JAVA/src/main/java/Pl2_interfaz/Login_Administrador.java
@@ -128,7 +128,7 @@ public class Login_Administrador extends javax.swing.JFrame {
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         // TODO add your handling code here:
-        Registro_Login nuevaVentana = new Registro_Login();
+        Login_User nuevaVentana = new Login_User();
     
         // Mostrar la nueva ventana
         nuevaVentana.setVisible(true);
--- PL2/PL2_JAVA/src/main/java/Pl2_interfaz/Login_User.java
+++ PL2/PL2_JAVA/src/main/java/Pl2_interfaz/Login_User.java
@@ -8,12 +8,12 @@ package Pl2_interfaz;
  *
  * @author Alejandro
  */
-public class Registro_Login extends javax.swing.JFrame {
+public class Login_User extends javax.swing.JFrame {
 
     /**
      * Creates new form Registro_Login
      */
-    public Registro_Login() {
+    public Login_User() {
         initComponents();
     }
 
@@ -169,20 +169,20 @@ public class Registro_Login extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Registro_Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Login_User.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Registro_Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Login_User.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Registro_Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Login_User.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Registro_Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Login_User.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Registro_Login().setVisible(true);
+                new Login_User().setVisible(true);
             }
         });
     }
